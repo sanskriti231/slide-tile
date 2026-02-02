a080e023127f3571ec30eec2bf7c2bf2c2705f16
sanskriti231 <78299901+sanskriti231@users.noreply.github.com>
2026-02-02 16:49:46 +0530

    BFS fix

--- version1/bfs.py
+++ version1/bfs.py
@@ -4,10 +4,11 @@ dirs = [[1,3],[0,2,4],[1,5],
         [0,4,6],[1,3,5,7],[2,4,8],
         [3,7],[6,4,8],[5,7]]
 
-def bfs(state, visited, goal):
-    if state == goal: return 0
+def bfs(state, goal):
+    if state == goal: return state, 0
     q = Queue()
     q.push([state, 0])
+    visited = set()
     visited.add(state)
 
     while not q.isEmpty():
@@ -22,4 +23,4 @@ def bfs(state, visited, goal):
             if newstate in visited: continue
             q.push([newstate, dist + 1])
             visited.add(newstate)
-    return -1
+    return None, -1
